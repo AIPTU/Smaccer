cc4edc40623994f5a215d97f408d989f52016c6a
AIPTU <anangp808@gmail.com>
2024-06-29 12:16:03 +0700

    refactor: move inventory and skins to traits

--- src/aiptu/smaccer/entity/HumanSmaccer.php
+++ src/aiptu/smaccer/entity/HumanSmaccer.php
@@ -16,7 +16,9 @@ namespace aiptu\smaccer\entity;
 use aiptu\smaccer\entity\trait\CommandTrait;
 use aiptu\smaccer\entity\trait\CreatorTrait;
 use aiptu\smaccer\entity\trait\EmoteTrait;
+use aiptu\smaccer\entity\trait\InventoryTrait;
 use aiptu\smaccer\entity\trait\RotationTrait;
+use aiptu\smaccer\entity\trait\SkinTrait;
 use aiptu\smaccer\entity\trait\SlapBackTrait;
 use aiptu\smaccer\entity\trait\VisibilityTrait;
 use aiptu\smaccer\entity\utils\EntityTag;
@@ -24,9 +26,7 @@ use aiptu\smaccer\Smaccer;
 use pocketmine\entity\Human;
 use pocketmine\entity\Location;
 use pocketmine\entity\Skin;
-use pocketmine\item\Item;
 use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\player\Player;
 
 class HumanSmaccer extends Human {
 	use CreatorTrait;
@@ -35,6 +35,8 @@ class HumanSmaccer extends Human {
 	use SlapBackTrait;
 	use EmoteTrait;
 	use CommandTrait;
+	use InventoryTrait;
+	use SkinTrait;
 
 	public function __construct(Location $location, Skin $skin, ?CompoundTag $nbt = null) {
 		if ($nbt instanceof CompoundTag) {
@@ -79,8 +81,8 @@ class HumanSmaccer extends Human {
 	protected function entityBaseTick(int $tickDiff = 1) : bool {
 		$hasUpdate = parent::entityBaseTick($tickDiff);
 
-		if ($this->emote !== null) {
-			$emoteUuid = $this->emote->getUuid();
+		if ($this->getEmote() !== null) {
+			$emoteUuid = $this->getEmote()->getUuid();
 
 			if (Smaccer::getInstance()->getDefaultSettings()->isEmoteCooldownEnabled()) {
 				if ($this->handleEmoteCooldown($emoteUuid)) {
@@ -94,90 +96,5 @@ class HumanSmaccer extends Human {
 		}
 
 		return $hasUpdate;
-	}
-
-	public function setArmor(Player $source) : void {
-		$armorContents = $source->getArmorInventory()->getContents();
-		$this->getArmorInventory()->setContents($armorContents);
-	}
-
-	public function setHelmet(Item|Player $source) : void {
-		$item = $source instanceof Player ? $source->getArmorInventory()->getHelmet() : $source;
-		$this->getArmorInventory()->setHelmet($item);
-	}
-
-	public function setChestplate(Item|Player $source) : void {
-		$item = $source instanceof Player ? $source->getArmorInventory()->getChestplate() : $source;
-		$this->getArmorInventory()->setChestplate($item);
-	}
-
-	public function setLeggings(Item|Player $source) : void {
-		$item = $source instanceof Player ? $source->getArmorInventory()->getLeggings() : $source;
-		$this->getArmorInventory()->setLeggings($item);
-	}
-
-	public function setBoots(Item|Player $source) : void {
-		$item = $source instanceof Player ? $source->getArmorInventory()->getBoots() : $source;
-		$this->getArmorInventory()->setBoots($item);
-	}
-
-	public function setOffHandItem(Item|Player $source) : void {
-		$offHandItem = $source instanceof Player ? $source->getOffHandInventory()->getItem(0) : $source;
-		$this->getOffHandInventory()->setItem(0, $offHandItem);
-	}
-
-	public function setItemInHand(Item|Player $source) : void {
-		$itemInHand = $source instanceof Player ? $source->getInventory()->getItemInHand() : $source;
-		$this->getInventory()->setItemInHand($itemInHand);
-	}
-
-	public function getArmor() : array {
-		return $this->getArmorInventory()->getContents();
-	}
-
-	public function getHelmet() : Item {
-		return $this->getArmorInventory()->getHelmet();
-	}
-
-	public function getChestplate() : Item {
-		return $this->getArmorInventory()->getChestplate();
-	}
-
-	public function getLeggings() : Item {
-		return $this->getArmorInventory()->getLeggings();
-	}
-
-	public function getBoots() : Item {
-		return $this->getArmorInventory()->getBoots();
-	}
-
-	public function getOffHandItem() : Item {
-		return $this->getOffHandInventory()->getItem(0);
-	}
-
-	public function getItemInHand() : Item {
-		return $this->getInventory()->getItemInHand();
-	}
-
-	public function changeSkin(string $skinData) : void {
-		$this->setSkin(new Skin(
-			$this->getSkin()->getSkinId(),
-			$skinData,
-			$this->getSkin()->getCapeData(),
-			$this->getSkin()->getGeometryName(),
-			$this->getSkin()->getGeometryData()
-		));
-		$this->sendSkin();
-	}
-
-	public function changeCape(string $capeData) : void {
-		$this->setSkin(new Skin(
-			$this->getSkin()->getSkinId(),
-			$this->getSkin()->getSkinData(),
-			$capeData,
-			$this->getSkin()->getGeometryName(),
-			$this->getSkin()->getGeometryData()
-		));
-		$this->sendSkin();
 	}
 }
