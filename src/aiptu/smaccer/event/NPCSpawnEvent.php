adad9f880e29aadb93465e19b90af7c8853d2214
AIPTU <anangp808@gmail.com>
2024-06-30 13:32:12 +0700

    feat: implements npc events

---
+++ src/aiptu/smaccer/event/NPCSpawnEvent.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Copyright (c) 2024 AIPTU
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/AIPTU/Smaccer
+ */
+
+declare(strict_types=1);
+
+namespace aiptu\smaccer\event;
+
+use pocketmine\entity\Entity;
+use pocketmine\event\entity\EntityEvent;
+
+/**
+ * @phpstan-extends EntityEvent<Entity>
+ */
+class NPCSpawnEvent extends EntityEvent {
+	public function __construct(protected Entity $entity) {}
+}
